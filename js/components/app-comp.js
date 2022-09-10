d943e2ae2ec11eac967b41eaf41a47597ff6c033
chenp <chenpeleg@gmail.com>
2022-09-10 20:44:48 +0300

    Updates

---
+++ js/components/app-comp.js
@@ -0,0 +1,46 @@
+
+
+const template = document.createElement('template');
+template.innerHTML = `
+  <style>
+    div {
+      margin-top: 20px;
+      color: green;
+    }
+  </style>
+  <div>
+    <p>The Google search result of your name is <a target="_blank" rel="noopener">here</a></p>
+  </div>
+`;
+
+class SearchResult extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+        /**@type  {ShadowRoot}*/
+        // @ts-ignore
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+        // @ts-ignore
+        this.shadowRoot.querySelector('a').href = '';
+    }
+
+    static get observedAttributes() {
+        return ['name-attribute'];
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (name == 'name-attribute') {
+            // @ts-ignore
+            this.shadowRoot.querySelector(
+                'a'
+            ).href = `https://www.google.com/search?q=${newValue}`;
+        }
+    }
+}
+
+window.customElements.define('search-result', SearchResult);
+
+export {
+
+
+}
---
+++ js/main.js
@@ -0,0 +1,21 @@
+import { Utils } from "./Utils/utils.js";
+import * as appComp from "./components/app-comp.js"
+export const a = () => {
+    const u = Utils
+    console.log(u.random(10))
+    class mainApp {
+        constructor() {
+
+        }
+    }
+    window.addEventListener("popstate", event => {
+        // Grab the history state id
+        //  let stateId = event.state.id;
+        // Show clicked id in console (just for fun)
+        console.log(window.location.hash);
+        // Visually select the clicked button/tab/box
+        //select_tab(stateId);
+        // Load content for this tab/page
+        //load_content(stateId);
+    });
+}
