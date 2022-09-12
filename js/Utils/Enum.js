a5d40811c0a308b1723c820508f4fe2687d8fb54
chenpeleg <chenpeleg@gmail.com>
2022-09-12 23:02:56 +0300

    enum validator function

--- js/Utils/Enum.js
+++ js/Utils/Enum.js
@@ -1,10 +1,41 @@
 
-/** 
- * @enum {number| string}
- *   
- */
+/**
+ * This is an enum validator function, that
+ * turns arrays and objects to enums.
+ * @param {Record<string,any> | Array<string>} enumObj
+ * @param {'number' | 'string'} type='number'
+*/
 export const Enum = (enumObj, type = 'number') => {
-    if (Array.isArray(enumObj)) {
 
+    if (Array.isArray(enumObj)) {
+        const newObj = {};
+        enumObj.forEach((el, i) => {
+            newObj[el] = type === 'number' ? i : el;
+        })
+        return newObj
     }
+    const keys = Object.keys(enumObj)
+    if (type === 'string') {
+        keys.forEach(key => {
+            enumObj[key] = key;
+        })
+        return enumObj;
+    }
+    const existingNumbers = /**@type {Set<number>} */ (new Set());
+    keys.forEach(key => {
+        const number = enumObj[key];
+        enumObj[key] = Number(number);
+        if (!existingNumbers.has(number)) {
+            existingNumbers.add(number)
+        } else {
+            enumObj[key] = NaN;
+        }
+        let maxNumber = [...existingNumbers].reduce((p, c) => p < c ? c : p);
+        keys.forEach(
+            key => enumObj[key] = isNaN(enumObj[key]) ? enumObj[key] : ++maxNumber
+        )
+        return enumObj;
+    })
+
+
 }
