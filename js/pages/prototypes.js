01c6ac933a0790eb5d84a294c4caa15dcaa90e55
chenpeleg <chenpeleg@gmail.com>
2022-09-17 23:14:20 +0300

    added styles and some prototype logic

--- js/pages/prototypes.js
+++ js/pages/prototypes.js
@@ -1,3 +1,4 @@
+import { BaseStyles } from "../styles/baseStyles.js";
 import { Utils } from "../Utils/utils.js";
 /** 
  * @typedef ProtoChainMember
@@ -33,21 +34,38 @@ export class ProtoTypes {
         return container;
     }
     renderChain() {
+        const oneChainElement = this.renderOnChainElement({})
+        this.container.appendChild(oneChainElement)
+        //chainMemberContainer.setStyle = {}
+
+    }
+    renderOnChainElement(element) {
         const chainMemberContainer = document.createElement('div');
         /**@type {import("../types/css-types.js").CSSObejct} */
         Utils.styler(chainMemberContainer, {
             height: '100px',
-            'background-color': 'red',
-            'width': '200px'
+            'background-color': '#AABBAA',
+            'width': '200px',
+            'border-radius': '4px',
+            'box-shadow': '2px 2px 4px rgba(0,0,0,.5)',
+            ...BaseStyles.flexCol
         })
-        this.container.appendChild(chainMemberContainer)
-        //chainMemberContainer.setStyle = {}
-
+        const chainMemeberHeader = document.createElement('H4');
+        chainMemeberHeader.innerHTML = 'String.Protoype'
+        chainMemberContainer.append(chainMemeberHeader)
+        return chainMemberContainer;
     }
     generateRandId(name) {
         return name + '_' + Utils.random(10000);
     }
     showChain(obj) {
+        /**@type {ProtoChainMember[]} */
+        const completeChain = this.buildChainObject(obj);
+        this.consoleLogChain(completeChain);
+
+    }
+    buildChainObject(obj) {
+
         const getProtoChaninToArray = (o, arr = []) => {
             arr.push(o)
             const proto = Object.getPrototypeOf(o);
@@ -84,10 +102,11 @@ export class ProtoTypes {
         const completeChain = protoArray.map((o, i) => {
             return { obj: o, level: (protoArray.length - i - 1), description: [o], specialType: getSpecialType(o) }
         });
+        return completeChain;
 
 
     }
-
+    /**@type {(completeChain : ProtoChainMember[])=> void} */
     consoleLogChain(completeChain) {
         completeChain.forEach((o) => {
             const text = "Level: " + o.level + " " + (o.specialType || o.description)
---
+++ js/styles/baseStyles.js
@@ -0,0 +1,11 @@
+
+
+/**@type {Record<string, import("../types/css-types.js").CSSObejct>} */
+export const BaseStyles = {
+    flexCol: {
+        display: 'flex',
+        'flex-direction': 'column',
+        'align-items': 'center'
+
+    }
+}
