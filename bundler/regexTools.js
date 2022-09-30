55ec7147e18bb525b9b75182605da18468ee5235
chenpeleg <chenpeleg@gmail.com>
2022-09-30 17:19:58 +1000

    TEST passing

--- bundler/regexTools.js
+++ bundler/regexTools.js
@@ -1,21 +1,24 @@
-export const RegexTools = {
+const RegexTools = {
     ImportRegex:
         /import(?:(?:(?:[ \n\t]+([^ *\n\t\{\},]+)[ \n\t]*(?:,|[ \n\t]+))?([ \n\t]*\{(?:[ \n\t]*[^ \n\t"'\{\}]+[ \n\t]*,?)+\})?[ \n\t]*)|[ \n\t]*\*[ \n\t]*as[ \n\t]+([^ \n\t\{\}]+)[ \n\t]+)from[ \n\t]*(?:['"])([^'"\n]+)(['"])/g,
+    ImportsNames: 4,
+    ImportRegexURLPath: 4,
 
-    getImportRegex: (txt) => {
-        const importRGX =
-            /import(?:(?:(?:[ \n\t]+([^ *\n\t\{\},]+)[ \n\t]*(?:,|[ \n\t]+))?([ \n\t]*\{(?:[ \n\t]*[^ \n\t"'\{\}]+[ \n\t]*,?)+\})?[ \n\t]*)|[ \n\t]*\*[ \n\t]*as[ \n\t]+([^ \n\t\{\}]+)[ \n\t]+)from[ \n\t]*(?:['"])([^'"\n]+)(['"])/g;
+    getImportRegex(txt) {
         const importLines = txt
             .split('import')
             .filter((t) => t)
             .map((t) => 'import' + t);
 
-        importLines.forEach((importLine, index) => {
+        importLines.map((importLine, index) => {
             const line = importLine.matchAll(importRGX);
-            console.log('\n\n', 'line ' + index, importLine);
-            for (const matchPart of line) {
-                console.log(matchPart);
-            }
+            // console.log('\n\n', 'line ' + index, importLine);
+            // for (const matchPart of line) {
+            //     console.log(matchPart);
+            // }
+            return { importLine };
         });
     },
 };
+
+export { RegexTools };
--- bundler/tests/js-file-class.test.js
+++ bundler/tests/js-file-class.test.js
@@ -3,7 +3,12 @@ import { JsFile } from '../js-file-class.js';
 describe('Js-file-class', () => {
     it('gets the import lines from file', () => {
         const text1 = `import * as fs from 'fs';
-import { RegexTools } from './regexTools.js';`;
+import { RegexTools } from './regexTools.js';import { PageModel } from "./models/page-model.js";
+import { RoutesEnum } from "./models/routes.js";
+import { About } from "./pages/about.js";
+import { Closure } from "./pages/closure.js";
+import { ProtoTypes } from "./pages/prototypes.js";
+`;
         const allImports = JsFile.imports2(text1);
     });
 });
